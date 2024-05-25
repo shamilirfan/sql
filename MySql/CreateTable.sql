8960f470ee7b0fde57dd66cf0c5868ab590a65f0
Shamil Irfan <shamilirfan28@gmail.com>
2024-05-25 16:07:44 +0600

    first commit

---
+++ MySql/CreateTable.sql
@@ -0,0 +1,8 @@
+create table student(
+    ID int primary key,
+    Roll int not null,
+    Name varchar(50) not null,
+    Institute varchar(60) not null,
+    City varchar(30)  null,
+    STK money not null
+);
