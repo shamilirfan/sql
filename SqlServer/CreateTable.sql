6091c6ddb7abccb8daef81e26bb499d86bc9df9c
Shamil Irfan <shamilirfan28@gmail.com>
2024-05-28 20:45:40 +0600

    ALL

--- SqlServer/CreateTable.sql
+++ SqlServer/CreateTable.sql
@@ -1,9 +1,24 @@
-create table students(
-    ID int primary key,
-    Roll int not null,
-    Students_Name varchar(50) not null,
-	Gender varchar(8) not null,
-    Institute varchar(60) not null,
-    City varchar(30)  null,
-    Scholarship_TK money not null
+-- table-1
+CREATE TABLE products(
+    Product_ID INT PRIMARY KEY,
+    Product_Name VARCHAR(50) NOT NULL,
+    Brand VARCHAR(30) NOT NULL,
+    Color VARCHAR(20) NULL,
+    StockIn DATE NOT NULL,
+    Price money
+);
+
+-- table-2
+CREATE TABLE customars(
+    Customar_ID INT PRIMARY KEY,
+    Customar_Name VARCHAR(40) NOT NULL,
+    Gender VARCHAR(6) NOT NULL,
+    City VARCHAR(20) NOT NULL,
+);
+
+-- table-3
+CREATE TABLE sales(
+    Product_ID INT FOREIGN KEY REFERENCES products(Product_ID),
+    Customar_ID INT FOREIGN KEY REFERENCES customars(Customar_ID),
+    Purches_Date DATE
 );
--- SqlServer/InsertData.sql
+++ SqlServer/InsertData.sql
@@ -1,16 +1,66 @@
-insert into sales(product_id, customar_id, purches_date)
-values (1, 1, '2020-01-01'),
-(2, 2, '2020-01-01'),
-(3, 3, '2021-01-01'),
-(4, 4, '2021-03-08'),
-(5, 5, '2021-07-15'),
-(6, 6, '2021-12-22'),
-(7, 7, '2022-11-30'),
-(8, 8, '2022-12-25'),
-(9, 9, '2023-06-19'),
-(10, 10, '2023-04-02'),
-(11, 11, '2023-03-22'),
-(12, 12, '2024-05-28'),
-(13, 13, '2024-01-30'),
-(14, 14, '2024-02-01'),
-(15, 15, '2024-03-17');
+-- insert for table-1
+INSERT INTO products(Product_ID, Product_Name, Brand, Color, StockIn, Price)
+VALUES (1, 'Smartwatch', 'Apple', 'Black', 3, 199),
+    (2, 'Smartwatch', 'Samsung', 'Silver', 30, 500),
+    (3, 'Cap', 'Reebok', NULL, 10, 560),
+    (4, 'Shoe', 'Reebok', 'White', 24, 2438),
+    (5, 'iPad', 'Apple', 'Silver', 45, 37953),
+    (6, 'MacBook', 'Apple', 'Black', 1, 9218),
+    (7, 'Galaxy S24 Ultra', 'Samsung', NULL, 3, 104000),
+    (8, 'Keyboard', 'Samsung', 'Black', 14, 500),
+    (9, 'iPhone 15 Pro', 'Apple', 'White', 8, 125000),
+    (10, 'Pixel 7', 'Google', NULL, 25, 300),
+    (11, 'Shoe', 'Nike', 'White', 7, 250),
+    (12, 'T-Shirt', 'Nike', 'Sky Blue', 10, 422),
+    (13, 'Bag', 'Nike', 'Black', 35, 657),
+    (14, 'MacBook', 'Apple', 'White', 1, 48971),
+    (15, 'Laptop', 'Samsung', 'Silver', 2, 3720);
+
+
+
+
+-- insert for table-2
+INSERT INTO customars(Customar_ID, Customar_Name, Gender, City)
+VALUES (1, 'Alice', 'Male', 'Khulna'),
+    (2, 'Nowshin', 'Female', 'Khulna'),
+    (3, 'Rupa', 'Male', 'Dhaka'),
+    (4, 'Bob', 'Male', 'Gazipur'),
+    (5, 'Himal', 'Male', 'Savar'),
+    (6, 'Nova', 'Female', 'Dhaka'),
+    (7, 'Nila', 'Female', 'Savar');
+    (8, 'Shanto', 'Male', 'Gazipur'),
+    (9, 'Topu', 'Male', 'Savar'),
+    (10, 'Anika', 'Female', 'Khulna'),
+    (11, 'Minar', 'Male', 'Dhaka'),
+    (12, 'Afraim', 'Male', 'Dhaka'),
+    (13, 'Ripon', 'Male', 'Sylhet'),
+    (14, 'Jerin', 'Female', 'Khulna'),
+    (15, 'Bijoy', 'Male', 'Gazipur'),
+    (16, 'Shagor', 'Male', 'Gazipur'),
+    (17, 'Mehzabin', 'Female', 'Savar');
+
+
+
+
+-- insert for table-3
+INSERT INTO sales(Product_ID, Customar_ID, Purches_Date)
+VALUES (1, 1, '2020-01-01'),
+    (2, 2, '2020-01-01'),
+    (3, 3, '2021-01-01'),
+    (4, 4, '2021-03-08'),
+    (5, 5, '2021-07-15'),
+    (6, 6, '2021-12-22'),
+    (7, 7, '2022-11-30'),
+    (8, 8, '2022-12-25'),
+    (9, 9, '2023-06-19'),
+    (10, 1, '2023-04-02'),
+    (11, 11, '2023-03-22'),
+    (12, 2, '2024-05-28'),
+    (13, 3, '2024-01-30'),
+    (14, 14, '2024-02-01'),
+    (15, 15, '2024-03-17');
+
+
+
+
+
